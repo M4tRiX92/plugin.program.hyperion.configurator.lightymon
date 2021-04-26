5f2f3d61a8b55f2128cdadbcade4ad315ae79c4a
M4tRiX92 <Simon.Montag@googlemail.com>
2021-04-26 21:29:08 +0200

    debugging

--- app.py
+++ app.py
@@ -13,4 +13,5 @@ def my_form_post():
     return 'You entered: {}'.format(request.form['text'])
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000, debug=False)
+    app.debug = True
+    app.run(host="0.0.0.0", port=8000)
