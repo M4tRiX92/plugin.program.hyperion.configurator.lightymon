0449134124c7c1789f900a2a1b2d5c59263021ca
M4tRiX92 <Simon.Montag@googlemail.com>
2021-04-27 21:41:01 +0200

    jinja2 settings

--- lightymon/__init__.py
+++ lightymon/__init__.py
@@ -4,8 +4,9 @@ from flask import Flask
 
 def create_app():
     """Construct the core Lightymon."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object("config.Config")
+    #app = Flask(__name__, instance_relative_config=False)
+    app = Flask(__name__)
+    #app.config.from_object("config.Config")
     #app.config["RECAPTCHA_PUBLIC_KEY"] = "iubhiukfgjbkhfvgkdfm"
     #app.config["RECAPTCHA_PARAMETERS"] = {"size": "100%"}
 
