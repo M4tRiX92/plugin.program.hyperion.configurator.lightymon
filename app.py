eaf724e15d651ce87c0ec6f8f82a80ab288c5d80
M4tRiX92 <Simon.Montag@googlemail.com>
2021-04-27 22:11:23 +0200

    changed

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 def my_form():
     return render_template('form.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def my_form_post():
     ledv = request.form['ledh']
     ledh = request.form['ledv']
     ledoption = request.form['index_option']
     #processed_text = text.upper()
-    return 'You entered: {}'.format(request.form['ledh'])
+    print("ledv" + ledv)
+    print("ledh" + ledh)
+    print("Option" + ledoption)
+    return 'Good'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=False)
