5b1af2951edf022af034398504f0abfa2424cb80
M4tRiX92 <Simon.Montag@googlemail.com>
2021-04-30 21:29:04 +0200

    new icon and new design

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 from flask import Flask, request, render_template
+from addon import 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_Lightymon():
-    return 'Welcome to Lightymon!'
+def index():
+    return render_template('index.html')
 
 @app.route('/setleds')
 def my_form():
-    return render_template('form.html')
+    return render_template('setleds.html')
 
-@app.route('/setleds', methods=['POST'])
+@app.route('/setleds', methods=['GET', 'POST'])
 def my_form_post():
-    ledv = request.form['ledv']
-    ledh = request.form['ledh']
-    direction = request.form['direction']
-    device = request.form['device']
-    center_corner = request.form['center_corner']
-    processed_text = "ledv " + ledv + " - ledh " + ledh + " - direction " + direction + " - device " + device + " - center_corner " + center_corner
-    #setEverything(ledh, ledv, options)
-    return processed_text
+if request.method == 'POST':
+        # do stuff when the form is submitted
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        ledv = request.form['ledv']
+        ledh = request.form['ledh']
+        direction = request.form['direction']
+        device = request.form['device']
+        center_corner = request.form['center_corner']
+        processed_text = "ledv " + ledv + " - ledh " + ledh + " - direction " + direction + " - device " + device + " - center_corner " + center_corner
+        #setEverything(ledh, ledv, options)
+        return processed_text
+
+    # show the form, it wasn't submitted
+    return render_template('setleds.html')
+    
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=False)
---
+++ app_old.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+from addon import 
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_Lightymon():
+    return 'Welcome to Lightymon!'
+
+@app.route('/setleds')
+def my_form():
+    return render_template('setleds.html')
+
+@app.route('/setleds', methods=['POST'])
+def my_form_post():
+    ledv = request.form['ledv']
+    ledh = request.form['ledh']
+    direction = request.form['direction']
+    device = request.form['device']
+    center_corner = request.form['center_corner']
+    processed_text = "ledv " + ledv + " - ledh " + ledh + " - direction " + direction + " - device " + device + " - center_corner " + center_corner
+    #setEverything(ledh, ledv, options)
+    return processed_text
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=False)
